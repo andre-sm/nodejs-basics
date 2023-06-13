64fb0021733bafa271e6c467abb3ea7447282a4f
--global <--global>
2023-06-14 00:37:00 +0300

    feat: implement create and copy functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import { mkdir, readdir, copyFile } from "fs/promises";
+import { existsSync } from "fs";
+import { fileURLToPath } from 'url';
+import { join, dirname } from "path";
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const originalPath = join(__dirname, 'files');
+    const copyPath = join(__dirname, 'files_copy');
+
+    try {
+        if (!existsSync(copyPath) && existsSync(originalPath)) {
+            await mkdir(copyPath, { recursive: true });
+        } else {
+            throw new Error('FS operation failed');
+        }
+
+        const files = await readdir(originalPath, { withFileTypes: true });
+
+        for (const file of files) { 
+            const orignalFilePath = join(originalPath, file.name);
+            const copyFilePath = join(copyPath, file.name);
+            await copyFile(orignalFilePath, copyFilePath);
+        }
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from "fs/promises";
+import { existsSync } from "fs";
+import { fileURLToPath } from 'url';
+import { join, dirname } from "path";
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        if (!existsSync(filePath)) {
+            await writeFile(filePath, 'I am fresh and young');
+        } else {
+            throw new Error('FS operation failed');
+        }
+    } catch(err) {
+        throw new Error(err.message);  
+    }
 };
 
 await create();
