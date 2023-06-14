c36db3258ad53f1611dcd3cf74e3f7629c129912
--global <--global>
2023-06-14 13:04:16 +0300

    feat: implement rename and delete functions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { unlink } from "fs/promises";
+import { existsSync } from "fs";
+import { fileURLToPath } from 'url';
+import { join, dirname } from "path";
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToRemove = join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        if (!existsSync(fileToRemove)) {
+            throw new Error('FS operation failed');
+        }
+        await unlink(fileToRemove);
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+import { existsSync } from "fs";
+import { fileURLToPath } from 'url';
+import { join, dirname } from "path";
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const oldPath = join(__dirname, 'files', 'wrongFilename.txt');
+    const newPath = join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        if (!existsSync(oldPath) || existsSync(newPath)) {
+            throw new Error('FS operation failed');
+        }
+        await fs.rename(oldPath, newPath);
+    } catch (err) {
+        throw new Error(err.message);
+    }
+
 };
 
 await rename();
