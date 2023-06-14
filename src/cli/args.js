38889b411a4f40673e0d8d42291849fa17940f44
--global <--global>
2023-06-14 18:38:13 +0300

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = [];
+    process.argv.slice(2).forEach((arg, i, arr) => {
+        if (i % 2 === 0) {
+            args.push(`${arg.slice(2)} is ${arr[i + 1]}`);
+        }
+    });
+    console.log(args.join(', '));
 };
 
 parseArgs();
