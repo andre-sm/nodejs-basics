583fc2059118fa2502445d2813f536f8d76005d9
--global <--global>
2023-06-15 23:36:23 +0300

    feat: calculate file hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { readFile } from "fs/promises";
+import { fileURLToPath } from 'url';
+import { join, dirname } from "path";
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const data = await readFile(filePath, { encoding: 'utf8' });
+        const hash = createHash('sha256').update(data).digest('hex');
+        console.log(hash);
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 await calculateHash();
