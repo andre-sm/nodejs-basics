4965c2728aacb6b029347a3717ae0d7217d87f73
--global <--global>
2023-06-18 00:12:14 +0300

    feat: implement child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { join, dirname } from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'script.js');
+
+    const childProcess = spawn('node', [filePath, ...args], {
+        stdio: ['pipe', 'pipe', 'pipe', 'ipc']  
+    });
+
+    childProcess.stdin.write(args.join(' '));
+
+    childProcess.stdout.on('data', data => {
+        process.stdout.write(data);
+    });
+      
+    childProcess.stderr.on('data', data => {
+        process.stderr.write(data);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
